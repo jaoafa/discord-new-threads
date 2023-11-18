0340d387367d7a860c726f11464aea09d0dc54ab
Tomachi <8929706+book000@users.noreply.github.com>
2023-11-18 13:21:12 +0900

    release: v2 (#348)
    * release: v2
    
    * fix: add ignore data dir

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+import { ConfigFramework } from '@book000/node-utils'
+
+export interface ConfigInterface {
+  discord: {
+    token: string
+    guildId: string
+    channelId: string
+  }
+}
+
+export class Configuration extends ConfigFramework<ConfigInterface> {
+  protected validates(): {
+    [key: string]: (config: ConfigInterface) => boolean
+  } {
+    return {
+      'discord is required': (config) => !!config.discord,
+      'discord.token is required': (config) => !!config.discord.token,
+      'discord.token must be a string': (config) =>
+        typeof config.discord.token === 'string',
+      'discord.guildId is required': (config) => !!config.discord.guildId,
+      'discord.guildId must be a string': (config) =>
+        typeof config.discord.guildId === 'string',
+      'discord.channelId is required': (config) => !!config.discord.channelId,
+      'discord.channelId must be a string': (config) =>
+        typeof config.discord.channelId === 'string',
+    }
+  }
+}
